4ac7d369f18fdda58d2de83037714d82794a3b2c
satya ranjan <academics.satyaranjan@gmail.com>
2023-04-14 11:27:50 +0530

    added terraform-workspace  on 13th april by 11912393

---
+++ terraform/terraform-workspace/terraform.tf
@@ -0,0 +1,19 @@
+terraform {
+
+  cloud {
+    organization = "satya-TF"
+
+    workspaces {
+      name = "terraform-cloud"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.28.0"
+    }
+  }
+
+  required_version = ">= 0.14.0"
+}
